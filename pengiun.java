3993a78b02b9385c3a58b00e2364841fb7c73d2f
JerryZhah <jiawei_zhan@aw.org>
2024-09-19 14:36:22 -0700

    Add files via upload

---
+++ pengiun.java
@@ -0,0 +1,29 @@
+class penguin {
+	String name;
+    String getNumPenguins
+    int getWeight
+    boolean open;
+
+public penguin(String name, String getAge, int getWeight, boolean open) 
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+    String getName;{
+        return Name
+    }
+    
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    double getNumPenguins() {
+        return NumPenguins;
+    }
+
+    public Penguin (String a, String n, int w)
+            name = a
+            number = n
+            weight = w
+}
