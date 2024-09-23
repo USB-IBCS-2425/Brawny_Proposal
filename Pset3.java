cb885baa520fdc670b4734675b6c9647081dd66d
JerryZhah <jiawei_zhan@aw.org>
2024-09-23 11:50:44 -0700

    Add files via upload

---
+++ Pset3.java
@@ -0,0 +1,38 @@
+public class Pset3 {
+    public static void main(String[] args) {
+    class Wallet {
+    private double money;
+    private String IDnumber;
+}
+
+    wallet mywallet = new wallet;
+    mywallet.setMoney = 1000000
+    mywallet.setIDnumber = 7207422
+    maywallet.payday = 100
+    mywallet.pay = 10
+    System.out.println(mywallet.getmoney);
+    System.out.println(mywallet.getidnumber)
+
+    public class Shape {
+  
+    double area;
+
+    public double getarea(){
+        return area;
+    }
+
+    public class Rectangle extends Shape {
+    int width;
+    int height;
+    public Rectangle(double width, double height) {
+    this.width = width;
+    this.height = height;
+     public double area() {
+        return width * height;
+
+    public class Circle extends Shape {
+    int radius
+    public Circle(double radius) {
+        this.radius = radius;
+    public double area() {
+        return raduis^2 = 3.14159
